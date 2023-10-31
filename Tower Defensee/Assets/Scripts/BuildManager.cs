6c2c07ed9a1683baeada95badc9575cfd389e088
Dylano28 <102656781+Dylano28@users.noreply.github.com>
2023-10-31 15:17:28 +0100

    removed code

--- Tower Defensee/Assets/Scripts/BuildManager.cs
+++ Tower Defensee/Assets/Scripts/BuildManager.cs
@@ -25,15 +25,4 @@ public class BuildManager : MonoBehaviour
     {
         selectedTower = _selectedTower;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
